0773b5cd0c869b31b69c1b300b149d92e05cd6ab
agus <agusalta002@gmail.com>
2024-05-20 21:37:42 -0300

    Agustin altamirano - upd

--- src/app/sales/Sale.jsx
+++ src/app/sales/Sale.jsx
@@ -4,9 +4,15 @@ import React from "react";
 function Sale(props) {
   return (
     <div className="flex flex-col items-center justify-center bg-stone-500 p-5 m-5 transition:smooth hover:scale-105 duration-75">
-      <h1>{props.storeLocation}</h1>
-      <p>{props.saleDate}</p>
-      <p>{props.purchaseMethod}</p>
+      <h1>
+        Ubiación de la tienda: <strong>{props.storeLocation}</strong>
+      </h1>
+      <p>
+        Fecha de compra: <strong>{props.saleDate}</strong>
+      </p>
+      <p>
+        Metodo de pago: <strong>{props.purchaseMethod}</strong>
+      </p>
       <Link
         className="text-red-950 font-bold hover:uppercase"
         href={`/sales/${props.id}`}
