34d457e42a98098c37d087935e86ef3a87869928
agus <agusalta002@gmail.com>
2024-05-20 21:42:21 -0300

    Agustin altamirano - updt estilos

--- src/app/customer/Customer.jsx
+++ src/app/customer/Customer.jsx
@@ -4,9 +4,9 @@ export default function Customer(props) {
   return (
     <div className="border p-3">
       <h1>Email: {props.email}</h1>
-      <p>Age: {props.age}</p>
-      <p>Satisfaction: {props.satisfaction}</p>
-      <p>Gender: {props.gender}</p>
+      <p>Edad: {props.age}</p>
+      <p>Satisfacción: {props.satisfaction}</p>
+      <p>Género: {props.gender}</p>
     </div>
   );
 }
