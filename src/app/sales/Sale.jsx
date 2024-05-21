505b234a0310ec28232f51cb627a5ac9dc97e366
martinbouhier <martinbouhier@outlook.com>
2024-05-20 21:59:58 -0300

    more styles

--- src/app/sales/Sale.jsx
+++ src/app/sales/Sale.jsx
@@ -1,22 +1,42 @@
 import Link from "next/link";
+import { FaDollarSign } from 'react-icons/fa';
 
 export default function Sale(props) {
   return (
-    <div className="flex flex-col items-center">
-      <li className="flex flex-col overflow-hidden rounded shadow-lg m-4 w-full max-w-md">
-        <div className="px-6 py-4">
-          <Link href={`sales/${props._id}/customer/`}>
-            <div className="font-bold text-xl mb-2">Customer</div>
-          </Link>
-          <Link href={`sales/${props._id}/items/`}>
-            
-            <div className="font-bold text-xl mb-2">Items</div>
-          </Link>
-          <p className="text-gray-700 text-base">Sale Date: {props.saleDate}</p>
-          <p className="text-gray-700 text-base">Store Location: {props.storeLocation}</p>
-          <p className="text-gray-700 text-base">Purchase Method: {props.purchaseMethod}</p>
+    <div className="flex justify-center items-center s-screen">
+    <div className="bg-white shadow-lg rounded-lg p-4 max-w-xs">
+      <div className="flex items-center mb-4">
+        <FaDollarSign size={50} className="mr-2 text-blue-500" />
+        <div>
+          <p className="text-sm text-gray-600">Sale Details</p>
         </div>
-      </li>
+      </div>
+      <div className="grid grid-cols-2 gap-2">
+        <div>
+          <Link href={`sales/${props._id}/customer/`}>
+            <div className="font-bold text-xl mb-2"><p className="text-gray-700 text-base">Customer</p></div>
+          </Link>
+        </div>
+        <div>
+        <Link href={`sales/${props._id}/items/`}>
+            <div className="font-bold text-xl mb-2"><p className="text-gray-700 text-base">Items</p></div>
+          </Link>
+        </div>
+        <div>
+          <p className="text-sm font-semibold text-gray-700">Sale Date: {props.saleDate}</p>
+        </div>
+        <div>
+          <p className="text-sm font-semibold text-gray-700">Store Location: {props.storeLocation}</p>
+        </div>
+        <div>
+          <p className="text-sm font-semibold text-gray-700">Purchase Method: {props.purchaseMethod}</p>
+        </div>
+      </div>
     </div>
+    </div>
+
+
   );
 }
+
+
